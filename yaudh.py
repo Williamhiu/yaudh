7db5688eebfbc6cf58f230369bf7895bcf5a8215
Williamhiu <125462687+Williamhiu@users.noreply.github.com>
2023-02-19 16:50:54 +0700

    Add files via upload

---
+++ yaudh.py
@@ -0,0 +1,13 @@
+import random
+import os
+import socket
+import threading
+
+os.system("clear")
+print ("YA KAMU,SAYA INGIN BILANG SAYA JOMBLO")
+os.system("clear")
+print ("SAYA SANGAT BOSAN KARNA SAYA JOMBLO BRUH")
+print ("KARNA SAYA JELEK :( FEK SMAIL")
+os.system("clear")
+print ("TUTOR GANTENG ANJING :v !!!!......")
+print ("UDAH GITU SEKARANG DAH MAU TAMAT SMP PENING MAU SEKOLAH DIMANA!")
